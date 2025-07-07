958fd742caf6542cc15bc492f19ed04ffb516992
Yuz <wwowkdrr@gmail.com>
2025-07-07 13:41:12 +0800

    製作新增學員表單

--- app2/api/get_students.php
+++ app2/api/get_students.php
@@ -4,5 +4,9 @@ $classroom=$_GET['classroom']??'101';
 
 $students=$Stu->all(['classroom'=>$classroom]);
 
+foreach($students as $key => $student){
+    $students[$key]['classname']=$ref[$student['classroom']];
+}
+
 header('Content-Type: application/json;');
 echo json_encode($students);
