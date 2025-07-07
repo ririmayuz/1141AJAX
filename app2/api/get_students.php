4d98abab66dadfee91967bda36f376931a3c8e89
Yuz <wwowkdrr@gmail.com>
2025-07-07 15:55:49 +0800

    使用ajax完成CRUD

--- app2/api/get_students.php
+++ app2/api/get_students.php
@@ -8,5 +8,6 @@ foreach($students as $key => $student){
     $students[$key]['classname']=$ref[$student['classroom']];
 }
 
+
 header('Content-Type: application/json;');
 echo json_encode($students);
---
+++ app2/api/save_student.php
@@ -0,0 +1,7 @@
+<?php include_once "db.php";
+
+$Stu->save($_POST);
+
+header("Content-Type: application/json;");
+echo json_encode(['success'=> true]);
+
